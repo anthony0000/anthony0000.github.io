3e91b0bbbf1a66963dcd8eaffb0bfd4bcfaadb67
phillips anthony <phillipsanthony2001@gmail.com>
2022-08-22 04:43:13 +0100

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from './components/NavBar.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+}
+export default App;
